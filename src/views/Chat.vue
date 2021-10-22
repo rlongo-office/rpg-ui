361dad379b0dc40d0c9c98ea78f485b2b9b81401
rlongo-dev <56315160+rlongo-dev@users.noreply.github.com>
2021-10-22 13:13:37 -0400

    git

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -10,6 +10,25 @@
 export default {
   name: 'chat',
   components: {'CharacterHeader':CharacterHeader,
-              }
+              },
+  methods: {
+
+  },
+    computed: {
+    isConnected(){
+      return this.$store.state.isConnected
+    },
+  },
+  created: ()=>{
+      if (this.isConnected){
+          let user = "Bob"
+          let type = "character";
+          let body =  "character load";
+          let dest = [user];
+          let msg = {id:0, type: type, data:body, dest:dest};
+          console.log("Loading character from character view");
+          this.$store.dispatch('sendMessage',msg);
+      }
+  }
 }
 </script>
