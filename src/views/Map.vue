3a69488ff2b1e60c7e723a1fc93e91e321987367
rlongo-dev <56315160+rlongo-dev@users.noreply.github.com>
2021-11-02 13:50:39 -0400

    new map image changes

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -3,13 +3,9 @@
       <h1>MAP PAGE</h1>
       <div class="portrait">
         <div
-          v-on:mousedown="handleMouseDown"
+          v-on:click ="handleMouseDown"
           v-on:mousemove="handleMouseMove"
-          v-on:mouseup="handleMouseUp"
           v-on:mouseleave="handleMouseLeave"
-          v-on:touchstart="handleMouseDown"
-          v-on:touchmove="handleMouseMove"
-          v-on:touchend="handleMouseUp"
           class="wrapper">
             <img
               v-on:load="imageLoad" 
@@ -29,7 +25,9 @@ export default {
     img: mapImage,
     imgTOP: 0,
     imgLEFT: 0,
+    isFirstPress: true,
     isDragging:false,
+    isScaling:false,
     divHeight:800,
     divWidth:800,
     topLimit:0,
@@ -41,8 +39,6 @@ export default {
   computed: {
 
     },
-  mounted() {
-  },
 methods: {
     
     imageLoad(){
@@ -51,27 +47,97 @@ methods: {
       this.topLimit = -(image.clientHeight-this.divHeight)
       this.leftLimit = -(image.clientWidth-this.divWidth)
     },
-    handleMouseDown(){
-      //let imgEl = this.$refs.img
-      //imgEl.style.left = 100;
-      //imgEl.style.top = 100;
-      if (this.imgTOP>this.topLimit){
-         this.imgTOP +=  -5;
-      }
-      if (this.imgLEFT>this.leftLimit){
-         this.imgLEFT +=  -5;
-      }
+    dist(e) {
+      let zw = e.touches[0].pageX - e.touches[1].pageX, zh = e.touches[0].pageY - e.touches[1].pageY;
+      return Math.sqrt(zw * zw + zh * zh);
     },
-    handleMouseMove(){
-
+    handleMouseDown(e){
+        let canMouseX;
+        let canMouseY;
+        if ((e.clientX)&&(e.clientY)) {
+          canMouseX = parseInt(e.clientX);
+          canMouseY = parseInt(e.clientY);
+          console.log("mouseDown: "+ canMouseX + "," + canMouseY)
+          // set the drag flag
+         this.isDragging=true;
+        } else if (e.targetTouches) {
+          e.preventDefault();
+          canMouseX=parseInt(e.targetTouches[0].clientX);
+          canMouseY=parseInt(e.targetTouches[0].clientY);
+          if (e.touches.length > 1) {
+            //detected a pinch
+            this.isScaling = true;
+          } else {
+            // set the drag flag
+            this.isDragging=true;
+            this.isScaling = false;
+          }
+        }
+        this.isFirstPress = false;
+        this.oldMouseX = canMouseX;
+        this.oldMouseY = canMouseY; 
     },
-    handleMouseUp(){
-
+    handleMouseMove(e){
+        let canMouseX
+        let canMouseY
+        console.log("mouseMove: "+ this.oldMouseX + "," 
+                                  + this.oldMouseY + "drag:" + this.isDragging)
+        if ((e.clientX)&&(e.clientY)) {
+          canMouseX = parseInt(e.clientX);
+          canMouseY = parseInt(e.clientY);
+        } else if (e.targetTouches) {
+          canMouseX = parseInt(e.targetTouches[0].clientX)
+          canMouseY = parseInt(e.targetTouches[0].clientY)
+          e.preventDefault()
+        }
+        //if scaling flag is set, clear the canvas and draw the image at the correct scale
+        if (this.isScaling){
+          this.imgScale = parseInt(this.dist(e)/this.touchDist);
+          if (this.imgScale < 1) this.imgScale=1; 
+        }
+        // if the drag flag is set, clear the canvas and draw the image
+        if(this.isDragging && !this.isFirstPress){
+            let yDiff = canMouseY - this.oldMouseY;
+            let xDiff = canMouseX - this.oldMouseX;
+            //this.subImgX += this.xDiff;
+            //this.subImgY += this.yDiff;
+            if(this.imgLEFT + xDiff <= this.leftLimit ){
+                xDiff = this.leftLimit
+            } else if(this.imgLEFT + xDiff >=0 ){
+               xDiff = 0
+            }
+            if(this.imgTOP + yDiff <= this.topLimit ){
+                yDiff = this.topLimit
+            } else if(this.imgTOP + yDiff >=0 ){
+               yDiff = 0
+            }
+            this.imgTOP += yDiff
+            this.imgLEFT += xDiff
+        }
+        this.oldMouseX = canMouseX;
+        this.oldMouseY = canMouseY;
+    },
+    handleMouseUp(e){
+        let canMouseX;
+        let canMouseY;
+        if ((e.clientX)&&(e.clientY)) {
+          canMouseX=parseInt(e.clientX-this.offsetX);
+          canMouseY=parseInt(e.clientY-this.offsetY);
+          // set the drag flag
+        } else if (e.targetTouches) {
+            e.preventDefault();
+            canMouseX=parseInt(e.targetTouches[0].clientX-this.offsetX);
+            canMouseY=parseInt(e.targetTouches[0].clientY-this.offsetY);
+          }
+        // user has removed touch or released mouse button, so clear the drag flag
+        this.isDragging = this.isScaling = false;
+        this.oldMouseX = canMouseX;
+        this.oldMouseY = canMouseY;
     },
     handleMouseLeave(){
-
+        // user has left the canvas, so clear the drag flag
+        this.isDragging = this.isScaling =false;
     }
-
   }
 }
 </script>
